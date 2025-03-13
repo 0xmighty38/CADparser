fcd679195be0fa80e2a3920ba3fe9e9355ea22ba
mighteagle <beyondissacnewton@gmail.com>
2025-03-13 13:58:15 -0400

    add publish folder

--- MaterialExtractor/obj/Release/net8.0/MaterialExtractor.AssemblyInfo.cs
+++ MaterialExtractor/obj/Release/net8.0/MaterialExtractor.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaterialExtractor")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8637d77e70c7a3cc7404c911031c990cb8bf81")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaterialExtractor")]
---
+++ publish/wwwroot/js/app.js
@@ -0,0 +1,28 @@
+﻿function extractMaterials() {
+    var fileInput = $("#cadFile")[0];
+    if (!fileInput.files[0]) {
+        alert("Please select a file.");
+        return;
+    }
+
+    var formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+
+    $.ajax({
+        url: "/api/materials/extract",
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            var tbody = $("#materialsTable tbody");
+            tbody.empty();
+            data.forEach(function (item) {
+                tbody.append(`<tr><td>${item.name}</td><td>${item.quantity}</td></tr>`);
+            });
+        },
+        error: function (xhr) {
+            alert("Error: " + xhr.responseText);
+        }
+    });
+}
