fa0d7f2ca4cdb98bdf58f5390ca9db402a7d7423
mighteagle <beyondissacnewton@gmail.com>
2025-03-14 00:42:25 -0400

    complete

--- MaterialExtractor/obj/Release/net8.0/MaterialExtractor.AssemblyInfo.cs
+++ MaterialExtractor/obj/Release/net8.0/MaterialExtractor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaterialExtractor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8637d77e70c7a3cc7404c911031c990cb8bf81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd679195be0fa80e2a3920ba3fe9e9355ea22ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaterialExtractor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaterialExtractor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
